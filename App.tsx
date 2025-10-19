58cccf5926a48b96b51525b09df641c2fc689fb0
Mohammad Talha <m.talhaleo2002@gmail.com>
2025-10-19 14:12:57 +0500

    Props With OnPress Event In React Native

--- App.tsx
+++ App.tsx
@@ -1,23 +1,25 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, Text, View } from 'react-native';
 
 const App = () => {
-  let name = 'Bruce Wayne';
+  const [name, setName] = useState('Smith Johnson');
   return (
     <View>
       <Text style={{ color: 'blue', fontSize: 40 }}>
         Props In React Native {name}
       </Text>
-      <User name={name} />
+      <Button title="Change Name" onPress={() => setName('Batman')} />
+      <User name={name} age={29} />
     </View>
   );
 };
 
-const User = (props: { name: string }) => {
+const User = (props: { name: string; age: any }) => {
   console.log(props.name);
   return (
     <View style={{ backgroundColor: 'purple' }}>
       <Text style={{ color: 'white', fontSize: 30 }}>{props.name}</Text>
+      <Text style={{ color: 'white', fontSize: 30 }}>{props.age}</Text>
     </View>
   );
 };
