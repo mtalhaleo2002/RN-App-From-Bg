3e01abcd2896e573f5e4eba7a10da8556addad89
Mohammad Talha <m.talhaleo2002@gmail.com>
2025-10-19 18:05:02 +0500

    Flat List In React Native

--- App.tsx
+++ App.tsx
@@ -1,82 +1,50 @@
-import React, { useState } from 'react';
-import { Text, View, StyleSheet, TextInput, Button } from 'react-native';
+import React from 'react';
+import { Text, View, StyleSheet, FlatList } from 'react-native';
 const App = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [display, setDisplay] = useState(false);
-
-  const resetForm = () => {
-    setName('');
-    setEmail('');
-    setPassword('');
-    setDisplay(false);
-  };
-
+  const users = [
+    {
+      name: 'Peter',
+      id: 1,
+    },
+    {
+      name: 'John',
+      id: 2,
+    },
+    {
+      name: 'Mary',
+      id: 3,
+    },
+    {
+      name: 'Steve',
+      id: 4,
+    },
+  ];
   return (
     <View>
       <Text style={{ fontSize: 25, backgroundColor: 'green', color: 'white' }}>
-        Simple Form In React Native
+        List with Flat List Component
       </Text>
-      <TextInput
-        placeholder="Enter User Name"
-        onChangeText={text => setName(text)}
-        value={name}
-        style={styles.textInput}
+
+      <FlatList
+        data={users}
+        renderItem={({ item }) => (
+          <Text style={styles.itemBox}>{item.name}</Text>
+        )}
       />
-
-      <TextInput
-        placeholder="Enter Password"
-        secureTextEntry={true}
-        onChangeText={text => setPassword(text)}
-        value={password}
-        style={styles.textInput}
-      />
-
-      <TextInput
-        placeholder="Enter User Email Address"
-        onChangeText={text => setEmail(text)}
-        value={email}
-        style={styles.textInput}
-      />
-
-      <View style={{ marginBottom: 10 }}>
-        <Button
-          title="Get Details"
-          color={'blue'}
-          onPress={() => setDisplay(true)}
-        />
-      </View>
-
-      <Button title="Clear Details" onPress={resetForm} />
-
-      <View>
-        {display ? (
-          <View>
-            <Text style={{ fontSize: 20, color: 'purple' }}>
-              User Name: {name}
-            </Text>
-            <Text style={{ fontSize: 20, color: 'purple' }}>
-              User Password: {password}
-            </Text>
-            <Text style={{ fontSize: 20, color: 'purple' }}>
-              User Email Address: {email}
-            </Text>
-          </View>
-        ) : null}
-      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  textInput: {
-    fontSize: 18,
-    margin: 10,
-    borderWidth: 3,
-    borderColor: 'gray',
-    borderRadius: 5,
+  itemBox: {
+    fontSize: 24,
+    padding: 10,
+    backgroundColor: 'blue',
     color: 'white',
+    margin: 10,
+    borderColor: 'gray',
+    borderWidth: 2,
+    borderRadius: 10,
   },
 });
 
