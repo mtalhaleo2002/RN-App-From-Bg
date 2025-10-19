37a8cdd7788c565eb34c0c15403751688293d7d7
Mohammad Talha <m.talhaleo2002@gmail.com>
2025-10-19 12:09:01 +0500

    Math Operations + Conditions in JSX

--- App.tsx
+++ App.tsx
@@ -1,16 +1,37 @@
 import React from 'react';
-import { Text, View, Button } from 'react-native';
+import { Text, View } from 'react-native';
 
+let name = 'John';
 const App = () => {
+  let name2 = 'JSX In Variable 2';
+  let lastName = 'Doe';
+  const age = 25;
   return (
     <View>
-      <Text style={{ fontSize: 30 }}>
-        Hello, World! <Text style={{ fontSize: 60 }}>Talha</Text>
+      <Text style={{ fontSize: 30 }}>JSX in React Native</Text>
+      <Text style={{ fontSize: 35 }}>{name}</Text>
+      <Text style={{ fontSize: 38 }}>{name2}</Text>
+      <Text style={{ fontSize: 40 }}>{age}</Text>
+      <Text style={{ fontSize: 43 }}>{10 * 20}</Text> {/* Math Operations */}
+      <Text style={{ fontSize: 46 }}>{name + ' ' + lastName}</Text>{' '}
+      {/* 2 variables merged  */}
+      <Text style={{ fontSize: 49 }}>
+        {name} {lastName}
       </Text>
-      <Button title="Press Me" color={'red'} />
-      <View>
-        <Text>Nested View Content</Text>
-      </View>
+      {/* 2 variables merged from a different approach  */}
+      <Text style={{ fontSize: 22 }}>
+        {age == 25 ? 'Age is 25' : 'Age is not 25'}
+      </Text>
+      {/* Condition Check */}
+      <Text style={{ fontSize: 22 }}>
+        {age < 25 ? `You're an underage` : `You're eligible`}
+      </Text>
+      {/* Again Condition Check  */}
+      {age >= 30 ? (
+        <Text style={{ fontSize: 22, color: 'red' }}>You can vote</Text>
+      ) : (
+        <Text style={{ fontSize: 22, color: 'green' }}>You cannot vote</Text>
+      )}
     </View>
   );
 };
