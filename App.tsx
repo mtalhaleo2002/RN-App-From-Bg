4c2891e8c57a45d45e71624f38b61fc4c4f96ff3
Mohammad Talha <m.talhaleo2002@gmail.com>
2025-10-19 12:31:08 +0500

    Components Structure And Use in App.tsx

--- App.tsx
+++ App.tsx
@@ -1,37 +1,34 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 
-let name = 'John';
 const App = () => {
-  let name2 = 'JSX In Variable 2';
-  let lastName = 'Doe';
-  const age = 25;
   return (
     <View>
-      <Text style={{ fontSize: 30 }}>JSX in React Native</Text>
-      <Text style={{ fontSize: 35 }}>{name}</Text>
-      <Text style={{ fontSize: 38 }}>{name2}</Text>
-      <Text style={{ fontSize: 40 }}>{age}</Text>
-      <Text style={{ fontSize: 43 }}>{10 * 20}</Text> {/* Math Operations */}
-      <Text style={{ fontSize: 46 }}>{name + ' ' + lastName}</Text>{' '}
-      {/* 2 variables merged  */}
-      <Text style={{ fontSize: 49 }}>
-        {name} {lastName}
-      </Text>
-      {/* 2 variables merged from a different approach  */}
-      <Text style={{ fontSize: 22 }}>
-        {age == 25 ? 'Age is 25' : 'Age is not 25'}
-      </Text>
-      {/* Condition Check */}
-      <Text style={{ fontSize: 22 }}>
-        {age < 25 ? `You're an underage` : `You're eligible`}
-      </Text>
-      {/* Again Condition Check  */}
-      {age >= 30 ? (
-        <Text style={{ fontSize: 22, color: 'red' }}>You can vote</Text>
-      ) : (
-        <Text style={{ fontSize: 22, color: 'green' }}>You cannot vote</Text>
-      )}
+      <Text style={{ color: 'blue', fontSize: 40 }}>Components</Text>
+      <UserData />
+      <CompanyData />
+    </View>
+  );
+};
+
+const UserData = () => {
+  return (
+    <View>
+      <Text style={{ fontSize: 25 }}>Name: John Doe</Text>
+      <Text style={{ fontSize: 25 }}>Age: 32</Text>
+      <Text style={{ fontSize: 25 }}>Email: something@gmail.com</Text>
+      <CompanyData />
+      {/* Component Can be used in another component also */}
+    </View>
+  );
+};
+
+const CompanyData = () => {
+  return (
+    <View>
+      <Text style={{ fontSize: 20 }}>Name: ABC Company</Text>
+      <Text style={{ fontSize: 20 }}>Total: 60 Employees </Text>
+      <Text style={{ fontSize: 20 }}>Product: XYZ Product</Text>
     </View>
   );
 };
