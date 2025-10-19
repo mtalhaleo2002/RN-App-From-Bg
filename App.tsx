aaad134f4e4882f2983875c49f9de1d3440f6dab
Mohammad Talha <m.talhaleo2002@gmail.com>
2025-10-19 16:49:09 +0500

    Text Inputs In React Native

--- App.tsx
+++ App.tsx
@@ -1,42 +1,35 @@
-import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
-import ExStyles from './style';
-
+import React, { useState } from 'react';
+import { Text, View, StyleSheet, TextInput, Button } from 'react-native';
 const App = () => {
+  const [name, setName] = useState('');
   return (
     <View>
-      <Text style={{ fontSize: 40, backgroundColor: 'purple', color: 'white' }}>
-        Style In React Native Text 1
+      <Text style={{ fontSize: 30, backgroundColor: 'green', color: 'white' }}>
+        Handle text Input
       </Text>
-      <Text style={styles.textBox}>Style In Text 2</Text>
-      <Text style={ExStyles.textBox2}>Style In Text 3</Text>
-      <Text style={[styles.boxText, ExStyles.textBox2, { width: 300 }]}>
-        Style In Text 4
+      <Text style={{ fontSize: 30, backgroundColor: 'brown', color: 'white' }}>
+        Your Name Is : {name}
       </Text>
+      <TextInput
+        style={styles.textInput}
+        placeholder="Enter Your Name"
+        value={name}
+        onChangeText={text => setName(text)}
+      />
+      <Button title="Clear Text Input" onPress={() => setName('')} />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  textBox: {
-    fontSize: 25,
-    backgroundColor: 'blue',
+  textInput: {
+    fontSize: 30,
+    backgroundColor: 'purple',
     color: 'white',
-    height: 100,
-    textAlign: 'center',
-    textAlignVertical: 'center',
-    marginBottom: 20,
-    borderColor: 'green',
-    borderWidth: 5,
-    borderRadius: 10,
-  },
-  boxText: {
-    fontFamily: 'Arial',
-    marginTop: 10,
-    borderBottomColor: 'red',
-    borderBottomWidth: 5,
-    borderBottomLeftRadius: 20,
-    borderBottomRightRadius: 20,
+    margin: 10,
+    padding: 10,
+    borderWidth: 2,
+    borderColor: 'white',
   },
 });
 
