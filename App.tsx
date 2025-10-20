32e9ef69a1ce5f1708b29d7e91e6c23b70ac9c5a
Mohammad Talha <m.talhaleo2002@gmail.com>
2025-10-20 12:37:48 +0500

    How to Make Grid

--- App.tsx
+++ App.tsx
@@ -1,61 +1,47 @@
 import React from 'react';
-import { Text, View, StyleSheet, ScrollView } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 const App = () => {
   const users = [
     {
-      name: 'Peter',
       id: 1,
+      name: 'Sam',
     },
-    {
-      name: 'John',
-      id: 2,
-    },
-    {
-      name: 'Mary',
-      id: 3,
-    },
-    {
-      name: 'Steve',
-      id: 4,
-    },
-    { name: 'Samantha', id: 5 },
-    { name: 'Paul', id: 6 },
-    { name: 'Laura', id: 7 },
-    { name: 'Tom', id: 8 },
-    { name: 'Diana', id: 9 },
-    { name: 'Lucy', id: 10 },
-    { name: 'Mark', id: 11 },
-    { name: 'Nina', id: 12 },
-    { name: 'Kevin', id: 13 },
-    { name: 'Olivia', id: 14 },
-    { name: 'Jake', id: 15 },
+    { id: 2, name: 'Anil' },
+    { id: 3, name: 'John' },
+    { id: 4, name: 'Doe' },
+    { id: 5, name: 'Sunny' },
+    { id: 6, name: 'Fing' },
+    { id: 7, name: 'Tony' },
+    { id: 8, name: 'Jago' },
+    { id: 9, name: 'Lara' },
+    { id: 10, name: 'Mia' },
   ];
   return (
     <View>
       <Text style={{ fontSize: 25, backgroundColor: 'green', color: 'white' }}>
-        List with Map Functions
+        How To Make Grid
       </Text>
-      <ScrollView>
-        {users.map(userNo => (
-          <Text key={userNo.id} style={styles.itemBox}>
-            {userNo.name}
-          </Text>
+      <View style={{ flex: 1, flexDirection: 'row', flexWrap: 'wrap' }}>
+        <Text style={styles.item}>Sam</Text>
+        {users.map(item => (
+          <Text style={styles.item}>{item.name}</Text>
         ))}
-      </ScrollView>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  itemBox: {
-    fontSize: 24,
-    padding: 10,
+  item: {
+    fontSize: 25,
+    padding: 5,
     backgroundColor: 'blue',
     color: 'white',
-    margin: 10,
-    borderColor: 'gray',
-    borderWidth: 2,
-    borderRadius: 10,
+    margin: 5,
+    height: 120,
+    width: 120,
+    textAlign: 'center',
+    textAlignVertical: 'center',
   },
 });
 
