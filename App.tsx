7e73b82ad0f01408699ea08b90e46de394d65676
Mohammad Talha <m.talhaleo2002@gmail.com>
2025-10-21 17:00:53 +0500

    State And Props In React Native

--- App.tsx
+++ App.tsx
@@ -3,9 +3,23 @@ import { Button, Text, View, TextInput } from 'react-native';
 import Student from './components/Student';
 
 class App extends Component {
-  fruit = () => {
-    console.log('Hello from fruit function');
-  };
+  constructor() {
+    super();
+    this.state = {
+      name: 'Anil',
+      age: 0,
+    };
+  }
+
+  updateState(val: string) {
+    this.setState({ name: val });
+
+    // this.setState({ name: 'React Native', age: 30 });
+    // This will update both name and age
+
+    // this.setState({ age: 20 });
+    // This is another approach to update state
+  }
   render() {
     return (
       <View
@@ -13,12 +27,13 @@ class App extends Component {
           marginTop: 20,
         }}
       >
-        <Text style={{ color: 'black', fontSize: 30 }}>
-          Class Component In React Native
-        </Text>
-        <TextInput placeholder="Enter Your Name" />
-        <Button title="Press" onPress={this.fruit} />
-        <Student />
+        <Text style={{ color: 'black', fontSize: 30 }}>{this.state.name}</Text>
+        <TextInput
+          placeholder="Enter Your Name"
+          onChangeText={text => this.updateState(text)}
+        />
+        <Button title="Press" />
+        <Student name={this.state.name} />
       </View>
     );
   }
--- components/Student.tsx
+++ components/Student.tsx
@@ -3,15 +3,14 @@ import { Text, View } from 'react-native';
 
 class Student extends Component {
   render() {
+    console.log(this.props.name);
     return (
       <View
         style={{
           marginTop: 20,
         }}
       >
-        <Text style={{ color: 'purple', fontSize: 30 }}>
-          Student Component In React Native
-        </Text>
+        <Text style={{ color: 'purple', fontSize: 30 }}>{this.props.name}</Text>
       </View>
     );
   }
