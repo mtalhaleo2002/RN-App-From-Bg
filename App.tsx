217d253cb5aa136b6cd828baecfddc6bb5bce2a1
Mohammad Talha <m.talhaleo2002@gmail.com>
2025-10-26 12:55:34 +0500

    useEffectHook As componentDidUpdate Life Cycle Method ( Specific State)

--- App.tsx
+++ App.tsx
@@ -3,19 +3,26 @@ import { Button, Text, View } from 'react-native';
 
 const App = () => {
   const [count, setCount] = useState(0);
+  const [data, setData] = useState(100);
 
   useEffect(() => {
-    // console.warn('Component Mounted');
-    console.log('Component Mounted');
-  }, []); // That method can be called only once when the component is mounted
+    console.log('Data is Updated');
+  }, [data]); // only when data changes
+
+  useEffect(() => {
+    console.log('Count is Updated');
+  }, [count]); // only when count changes
 
   return (
     <View>
       <Text style={{ color: 'black', fontSize: 30, marginTop: 30 }}>
-        Life Cycle with Use Effect {count}
+        useEffects as componentDidUpdate
       </Text>
+      <Text style={{ color: 'purple', fontSize: 30 }}>Count: {count}</Text>
+      <Text style={{ color: 'purple', fontSize: 30 }}>Data: {data}</Text>
 
       <Button title="Update Counter" onPress={() => setCount(count + 1)} />
+      <Button title="Update Counter" onPress={() => setData(data + 1)} />
     </View>
   );
 };
