e39312748693add302eed15e3a8951af82e753c0
Mohammad Talha <m.talhaleo2002@gmail.com>
2025-10-26 12:21:17 +0500

    useEffect Hook As Life Cycle Method In Functional Component

--- App.tsx
+++ App.tsx
@@ -1,42 +1,23 @@
-import React, { Component } from 'react';
-import { Button, Text, View, TextInput } from 'react-native';
-import Student from './components/Student';
+import React, { useEffect, useState } from 'react';
+import { Button, Text, View } from 'react-native';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: 'Anil',
-      age: 0,
-    };
-  }
+const App = () => {
+  const [count, setCount] = useState(0);
 
-  updateState(val: string) {
-    this.setState({ name: val });
+  useEffect(() => {
+    // console.warn('Component Mounted');
+    console.log('Component Mounted');
+  }, []); // That method can be called only once when the component is mounted
 
-    // this.setState({ name: 'React Native', age: 30 });
-    // This will update both name and age
+  return (
+    <View>
+      <Text style={{ color: 'black', fontSize: 30, marginTop: 30 }}>
+        Life Cycle with Use Effect {count}
+      </Text>
 
-    // this.setState({ age: 20 });
-    // This is another approach to update state
-  }
-  render() {
-    return (
-      <View
-        style={{
-          marginTop: 20,
-        }}
-      >
-        <Text style={{ color: 'black', fontSize: 30 }}>{this.state.name}</Text>
-        <TextInput
-          placeholder="Enter Your Name"
-          onChangeText={text => this.updateState(text)}
-        />
-        <Button title="Press" />
-        <Student name={this.state.name} />
-      </View>
-    );
-  }
-}
+      <Button title="Update Counter" onPress={() => setCount(count + 1)} />
+    </View>
+  );
+};
 
 export default App;
