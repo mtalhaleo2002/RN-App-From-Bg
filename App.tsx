b4e32a97ab4dacfc07d70f152a2dbee387871f88
Mohammad Talha <m.talhaleo2002@gmail.com>
2025-10-26 17:41:34 +0500

    Make Radio Button In React Native

--- App.tsx
+++ App.tsx
@@ -1,56 +1,61 @@
-import React from 'react';
-import { View, StyleSheet, TouchableHighlight, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet, Text, TouchableOpacity } from 'react-native';
 
 const App = () => {
+  const [selectedRadio, setSelectedRadio] = useState(1);
+
   return (
-    <View style={styles.container}>
-      <TouchableHighlight>
-        <Text style={[styles.button, styles.primary]}>Primary</Text>
-      </TouchableHighlight>
-      <TouchableHighlight>
-        <Text style={[styles.button, styles.success]}>Success</Text>
-      </TouchableHighlight>
-      <TouchableHighlight>
-        <Text style={[styles.button, styles.warning]}>Warning</Text>
-      </TouchableHighlight>
-      <TouchableHighlight>
-        <Text style={[styles.button, styles.error]}>Error</Text>
-      </TouchableHighlight>
-      <TouchableHighlight>
-        <Text style={styles.button}>Button</Text>
-      </TouchableHighlight>
+    <View style={styles.main}>
+      <TouchableOpacity onPress={() => setSelectedRadio(1)}>
+        <View style={styles.wrapper}>
+          <View style={styles.radio}>
+            {selectedRadio == 1 ? <View style={styles.radioBG}></View> : null}
+          </View>
+          <Text style={styles.radioText}>Radio 1</Text>
+        </View>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={() => setSelectedRadio(2)}>
+        <View style={styles.wrapper}>
+          <View style={styles.radio}>
+            {selectedRadio == 2 ? <View style={styles.radioBG}></View> : null}
+          </View>
+          <Text style={styles.radioText}>Radio 2</Text>
+        </View>
+      </TouchableOpacity>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
+  main: {
     flex: 1,
-    marginTop: 30,
+    backgroundColor: 'orange',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  button: {
-    backgroundColor: 'blue',
-    fontSize: 24,
+  radioText: {
+    fontSize: 30,
+    color: 'Black',
+  },
+  radio: {
+    height: 40,
+    width: 40,
+    borderColor: 'black',
+    borderRadius: 20,
+    borderWidth: 3,
     margin: 10,
-    padding: 10,
-    borderRadius: 10,
-    color: 'white',
-    textAlign: 'center',
-    shadowColor: '#000',
-    shadowOpacity: 1,
-    elevation: 10,
   },
-  primary: {
-    backgroundColor: 'blue',
+  wrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
   },
-  success: {
-    backgroundColor: 'green',
-  },
-  warning: {
-    backgroundColor: 'gold',
-  },
-  error: {
-    backgroundColor: 'red',
+  radioBG: {
+    height: 28,
+    width: 28,
+    backgroundColor: 'black',
+    borderRadius: 20,
+    margin: 3,
   },
 });
 
