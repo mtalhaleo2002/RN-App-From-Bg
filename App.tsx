98f9659220c7df9e74100164cc8c2c1f3b9cf0b3
Mohammad Talha <m.talhaleo2002@gmail.com>
2025-10-27 16:16:16 +0500

    Install NPM Packages In React Native

--- App.tsx
+++ App.tsx
@@ -1,48 +1,11 @@
-import React, { useState } from 'react';
-import { View, StyleSheet, Text, Button, Platform } from 'react-native';
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 const App = () => {
-  return (
-    <View style={styles.main}>
-      <Text style={styles.mainText}>Platform :{Platform.OS}</Text>
-      {Platform.OS == 'android' ? (
-        <View
-          style={{ height: 100, width: 100, backgroundColor: 'blue' }}
-        ></View>
-      ) : (
-        <View
-          style={{ height: 100, width: 100, backgroundColor: 'green' }}
-        ></View>
-      )}
-
-      <Text style={styles.text}>Code Logically</Text>
-      <Text style={styles.text}>
-        {JSON.stringify(Platform.constants.reactNativeVersion.minor)}
-      </Text>
-      <Text style={styles.text}>
-        {JSON.stringify(Platform.constants.reactNativeVersion)}
-      </Text>
-      <Text style={styles.text}>{JSON.stringify(Platform.constants)}</Text>
-    </View>
-  );
+  return <WebView source={{ uri: 'https://www.google.com/' }} />;
 };
 
-const styles = StyleSheet.create({
-  main: {
-    backgroundColor: 'orange',
-    flex: 1,
-    marginTop: 30,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  mainText: {
-    fontSize: 24,
-    color: 'white',
-  },
-  text: {
-    fontSize: 30,
-    color: Platform.OS === 'android' ? 'purple' : 'red',
-  },
-});
+const styles = StyleSheet.create({});
 
 export default App;
