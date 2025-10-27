3c00d3043453fda36816142498bcc5ded5beabd3
Mohammad Talha <m.talhaleo2002@gmail.com>
2025-10-27 16:40:11 +0500

    Make Custom Dialog Box

--- App.tsx
+++ App.tsx
@@ -1,11 +1,47 @@
-import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
-import { WebView } from 'react-native-webview';
+import React, { useState } from 'react';
+import { View, StyleSheet, Button, Text } from 'react-native';
 
 const App = () => {
-  return <WebView source={{ uri: 'https://www.google.com/' }} />;
+  const [show, setShow] = useState(false);
+
+  return (
+    <View style={styles.container}>
+      {show ? (
+        <View style={styles.modal}>
+          <View style={styles.subModalBody}>
+            <Text>Content above the button</Text>
+          </View>
+          <Button title="Close Dialog" onPress={() => setShow(false)} />
+        </View>
+      ) : null}
+
+      <Button title="Open Dialog" onPress={() => setShow(true)} />
+    </View>
+  );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    // alignItems: 'center',
+  },
+  modal: {
+    flex: 1,
+    backgroundColor: 'rgba(50,50,50,0.5)',
+    padding: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  subModalBody: {
+    backgroundColor: 'white',
+    width: 300,
+    height: 200,
+    padding: 30,
+    marginBottom: 20,
+    borderRadius: 10,
+    justifyContent: 'center',
+  },
+});
 
 export default App;
