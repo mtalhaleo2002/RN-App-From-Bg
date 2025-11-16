8962e6e97aa0db73eaff7b3dfdbc93e388a5487d
Mohammad Talha <m.talhaleo2002@gmail.com>
2025-11-16 16:57:44 +0500

    Fetch Data from JSON Server API

--- App.tsx
+++ App.tsx
@@ -1,50 +1,52 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, FlatList } from 'react-native';
-
-interface Post {
-  userId: number;
-  id: number;
-  title: string;
-  body: string;
-}
+import { View, Text } from 'react-native';
 
 const App = () => {
-  const [data, setData] = useState<Post[] | null>([]);
+  const [data, setData] = useState<User[]>([]);
+
+  type User = {
+    id: number;
+    name: string;
+    age: number;
+    email: string;
+  };
+
   const getAPIData = async () => {
-    const url = 'https://jsonplaceholder.typicode.com/posts';
+    console.log('API Call');
+    const url = 'http://10.0.2.2:3000/users'; // Emulator ke liye 100% correct
     let result = await fetch(url);
-    const response: Post[] = await result.json();
-    console.log(result);
-    setData(response);
+    const json: User[] = await result.json();
+    console.log(json);
+    setData(json);
   };
 
   useEffect(() => {
     getAPIData();
   }, []);
   return (
-    <View style={{ marginTop: 50 }}>
-      <Text style={{ fontSize: 30 }}>FlatList With API Data</Text>
-
-      {data?.length ? (
-        <FlatList
-          data={data}
-          renderItem={({ item }) => (
-            <View
-              style={{
-                borderBottomColor: 'orange',
-                borderBottomWidth: 2,
-                padding: 10,
-              }}
-            >
-              <Text style={{ fontSize: 30, backgroundColor: 'gray' }}>
-                {item.id}
-              </Text>
-              <Text style={{ fontSize: 24 }}>{item.title}</Text>
-              <Text style={{ fontSize: 18 }}>{item.body}</Text>
-            </View>
-          )}
-        />
-      ) : null}
+    <View
+      style={{
+        paddingTop: 50,
+        backgroundColor: 'white',
+      }}
+    >
+      <Text style={{ fontSize: 30 }}>Call JSON Server API</Text>
+      {data.map(item => (
+        <View
+          key={item.id}
+          style={{
+            borderWidth: 1,
+            borderColor: 'grey',
+            margin: 10,
+            padding: 10,
+          }}
+        >
+          <Text>ID: {item.id}</Text>
+          <Text>Name: {item.name}</Text>
+          <Text>Age: {item.age}</Text>
+          <Text>Email: {item.email}</Text>
+        </View>
+      ))}
     </View>
   );
 };
